47f289aba69114303a962c072ae2952ab241b653
Yuri Botelho <yurihaury@outlook.com>
2022-12-21 18:05:58 -0300

    Adição de funções de interação com o usuário

--- src/MegaSena.java
+++ src/MegaSena.java
@@ -1,8 +1,15 @@
 //Biblioteca responsável por gerar números aleatórios
 import java.util.Random;
+import java.util.Scanner;
 
 public class MegaSena {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String name = scanner.nextLine();
+        System.out.println("Olá, " +name+"! Aqui estão os números da Mega Sena:");
+
         //Objeto responsável por gerar números aleatórios
         Random generated = new Random();
 
